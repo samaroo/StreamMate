4a5fda792facb92e54efeb4cd1f5be95310c1295
Brandon Samaroo <samaroo@umich.edu>
2020-08-12 23:06:00 -0400

    Added streaming functionality

--- streammate_client/src/Components/StreamComponents/StreamView.js
+++ streammate_client/src/Components/StreamComponents/StreamView.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 
+//flv.hljs
+import flv from 'flv.js'
+
 //import action creators
 import {fetchAStream} from '../../Redux/ActionCreators/StreamManipulation/FetchAStream'
 import {loadStreamViewPage} from '../../Redux/ActionCreators/LoadStreamViewPage'
 
 class StreamView extends Component {
+    constructor(props){
+        super(props);
+        this.videoRef = React.createRef();
+    }
+
     componentDidMount() {
         this.props.fetchAStream(this.props.match.params.id);
         this.props.loadStreamViewPage();
+        this.constructFLVPlayer();
+    }
+
+    componentDidUpdate() {
+        this.constructFLVPlayer();
+    }
+
+    constructFLVPlayer = () => {
+        if(!this.props.stream){
+            return;
+        }
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:3002/live/${this.props.match.params.id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     //render functions
@@ -17,8 +42,8 @@ class StreamView extends Component {
         if(this.props.stream){
             console.log(this.props.stream)
             return(
-                <div>
-                    <h1>{this.props.stream.Title}</h1>
+                <div style={{marginTop: '10px'}}>
+                    <h2>{this.props.stream.Title}</h2>
                     <h5>{this.props.stream.Description}</h5>
                 </div>
             );
@@ -33,7 +58,8 @@ class StreamView extends Component {
 
     render() {
         return (
-            <div style={{marginTop: '50px'}}>
+            <div style={{marginTop: '-40px'}}>
+                <video controls style={{width: '90%', marginTop: '50px', borderRadius: '10px'}} ref={this.videoRef}/>
                 {this.renderStreamInfo()}
             </div>
         )
